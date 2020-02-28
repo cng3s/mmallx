290e4f9ecd58d3dec8856ebeb2abdeafe45d4f56
chneg <chneg33@outlook.com>
2020-02-28 14:06:34 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 echo "===========进入git项目happymmall目录============="
-cd /developer/git-repository/mmall
+cd /root/mmallx
 echo "==========git切换分之到mmall-v1.0==============="
-git checkout mmall-v1.0
+git checkout master
 echo "==================git fetch======================"
 git fetch
 echo "==================git pull======================"
@@ -9,13 +9,16 @@ git pull
 echo "===========编译并跳过单元测试===================="
 mvn clean package -Dmaven.test.skip=true
 echo "============删除旧的ROOT.war==================="
-rm /developer/apache-tomcat-7.0.73/webapps/ROOT.war
+rm /webapps/tomcat-7/webapps/ROOT.war
 echo "======拷贝编译出来的war包到tomcat下-ROOT.war======="
-cp /developer/git-repository/mmall/target/mmall.war /developer/apache-tomcat-7.0.73/webapps/ROOT.war
+cp /root/mmallx/target/mmall.war /webapps/tomcat-7/webapps/ROOT.war
 echo "============删除tomcat下旧的ROOT文件夹============="
-rm -rf /developer/apache-tomcat-7.0.73/webapps/ROOT
+rm -rf /webapps/tomcat-7/webapps/ROOT
 echo "====================关闭tomcat====================="
-/developer/apache-tomcat-7.0.73/bin/shutdown.sh
+/webapps/tomcat-7/bin/shutdown.sh
+
+# sleep 10s 等待spring项目关闭
+# 如果项目比较大那么需要sleep 更多的时间
 echo "================sleep 10s========================="
 for i in {1..10}
 do
@@ -23,4 +26,4 @@ do
     sleep 1s
 done
 echo "====================启动tomcat====================="
-/developer/apache-tomcat-7.0.73/bin/startup.sh
+/webapps/tomcat-7/bin/startup.sh
