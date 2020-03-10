cada55fdd16d4527bdc4dfe75a890813b4f1a258
chneg <chneg33@outlook.com>
2020-03-10 17:29:33 +0800

    ProductController的URL使用RESTful风格

--- src/main/java/com/mmall/controller/portal/ProductController.java
+++ src/main/java/com/mmall/controller/portal/ProductController.java
@@ -4,11 +4,10 @@ import com.github.pagehelper.PageInfo;
 import com.mmall.common.ServerResponse;
 import com.mmall.service.IProductService;
 import com.mmall.vo.ProductDetailVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/product/")
@@ -17,9 +16,18 @@ public class ProductController {
     @Autowired
     private IProductService iProductService;
 
+    // 不使用RESTful风格的URI: www.imooc11.com/product/detail.do?productId=26
     @RequestMapping("detail.do")
     @ResponseBody
     public ServerResponse<ProductDetailVo> detail(Integer productId) {
+        return iProductService.getProductDetail(productId);
+    }
+
+    // 使用RESTful风格的URI： www.imooc11.com/product/26
+    // 注：不是所有URI都适合改造成RESTful风格的,如显示数据内容比较适合RESTful,添加就不适合
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<ProductDetailVo> detailRESTful(@PathVariable Integer productId) {
         return iProductService.getProductDetail(productId);
     }
 
@@ -36,4 +44,84 @@ public class ProductController {
             , @RequestParam(value = "orderBy", defaultValue = "") String orderBy) {
         return iProductService.getProductByKeywordCategory(keyword, categoryId, pageNum, pageSize, orderBy);
     }
+
+    // list.do需要三种情况的RESTful URL 判断： 1. keyword 和 category 都不为空的；2. keyword 为空的；3. categoryId 为空的
+    @RequestMapping(value = "/{keyword}/{categoryId}/{pageNum}/{pageSize}/{orderBy}", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "keyword") String keyword
+            , @PathVariable(value = "categoryId") Integer categoryId
+            , @PathVariable(value = "pageNum") Integer pageNum
+            , @PathVariable(value = "pageSize") Integer pageSize
+            , @PathVariable(value = "orderBy") String orderBy) {
+
+        if (pageNum == null) {
+            pageNum = 1;
+        }
+        if (pageSize == null) {
+            pageSize = 10;
+        }
+        if (StringUtils.isBlank(orderBy)) {
+            orderBy = "price_asc";
+        }
+        return iProductService.getProductByKeywordCategory(keyword, categoryId, pageNum, pageSize, orderBy);
+    }
+
+    // http://www.imooc11.com/product/keyword/手机/1/10/price_asc
+    @RequestMapping(value = "/keyword/{keyword}/{pageNum}/{pageSize}/{orderBy}", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "keyword") String keyword
+            , @PathVariable(value = "pageNum") Integer pageNum
+            , @PathVariable(value = "pageSize") Integer pageSize
+            , @PathVariable(value = "orderBy") String orderBy) {
+        if (pageNum == null) {
+            pageNum = 1;
+        }
+        if (pageSize == null) {
+            pageSize = 10;
+        }
+        if (StringUtils.isBlank(orderBy)) {
+            orderBy = "price_asc";
+        }
+        return iProductService.getProductByKeywordCategory(keyword, null, pageNum, pageSize, orderBy);
+    }
+
+    // http://www.imooc11.com/product/category/100012/1/10/price_asc
+    @RequestMapping(value = "/category/{categoryId}/{pageNum}/{pageSize}/{orderBy}", method = RequestMethod.GET)
+    @ResponseBody
+    public ServerResponse<PageInfo> listRESTful(@PathVariable(value = "categoryId") Integer categoryId
+            , @PathVariable(value = "pageNum") Integer pageNum
+            , @PathVariable(value = "pageSize") Integer pageSize
+            , @PathVariable(value = "orderBy") String orderBy) {
+
+        if (pageNum == null) {
+            pageNum = 1;
+        }
+        if (pageSize == null) {
+            pageSize = 10;
+        }
+        if (StringUtils.isBlank(orderBy)) {
+            orderBy = "price_asc";
+        }
+        return iProductService.getProductByKeywordCategory("", categoryId, pageNum, pageSize, orderBy);
+    }
+
+    // 这是错误的 RESTful 风格的URL，因为如果keyword为空则URI定位会出现问题
+//    @RequestMapping(value = "/{keyword}/{pageNum}/{pageSize}/{orderBy}",method = RequestMethod.GET)
+//    @ResponseBody
+//    public ServerResponse<PageInfo> listRESTfulBadcase(@PathVariable(value = "keyword")String keyword,
+//                                                       @PathVariable(value = "pageNum") Integer pageNum,
+//                                                       @PathVariable(value = "pageSize") Integer pageSize,
+//                                                       @PathVariable(value = "orderBy") String orderBy){
+//        if(pageNum == null){
+//            pageNum = 1;
+//        }
+//        if(pageSize == null){
+//            pageSize = 10;
+//        }
+//        if(StringUtils.isBlank(orderBy)){
+//            orderBy = "price_asc";
+//        }
+//
+//        return iProductService.getProductByKeywordCategory(keyword,null,pageNum,pageSize,orderBy);
+//    }
 }
